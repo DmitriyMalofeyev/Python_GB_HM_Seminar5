8b25afea46501973258569059b5fdd9acc6686d0
DmitriyMalofeyev <et5@list.ru>
2022-09-22 19:03:49 +0300

    добавлено решение задачи 3 (RLE)

---
+++ HW/Task3/RLE.py
@@ -0,0 +1,43 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах
+
+
+
+
+with open('file1.txt', 'r') as data:
+    input_text = data.read()
+
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
+    count = 1
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    return str_code
+
+            
+str_code = encode_rle(input_text)
+print(str_code)
+
+with open('file2.txt', 'r') as data:
+    input_text2 = data.read()
+
+def decoding_rle(ss:str):
+    count = ''
+    str_decode = ''
+    for char in ss:
+        if char.isdigit():
+            count += char 
+        else:
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
+
+str_decode = decoding_rle(input_text2)
+print(str_decode)
